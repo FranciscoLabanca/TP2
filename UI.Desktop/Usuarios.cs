e58faaa6867431d36c162d0025c42cedef53e72f
Ignacio Bianchi <ebianchimat@frro.utn.edu.ar>
2019-07-22 17:26:12 -0300

    Lab04 terminado

---
+++ UI.Desktop/Usuarios.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Entities;
+using Business.Logic;
+
+
+
+namespace UI.Desktop
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvUsuarios.DataSource = ul.GetAll();
+
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TsbNuevo_Click(object sender, EventArgs e)
+        {
+            UsuarioDesktop ud = new UsuarioDesktop(ApplicationForm.ModoForm.Alta);
+            ud.ShowDialog();
+            this.Listar();
+        }
+
+        private void TsbEditar_Click(object sender, EventArgs e)
+        {
+            UsuarioDesktop ud = new UsuarioDesktop(((Usuario)dgvUsuarios.SelectedRows[0].DataBoundItem).ID,ApplicationForm.ModoForm.Modificacion);
+            ud.ShowDialog();
+            this.Listar();
+        }
+    }
+}
