f45c88a16f4c9363400ab2dbcd278818f1efcbc2
Martinpereyra <pereyramartinn@gmail.com>
2020-03-01 17:09:23 -0800

    abm planes

---
+++ UI.Web/Materias.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="Materias" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Materias.aspx.cs" Inherits="UI.Web.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="False"
+            SelectedRowStyle-BackColor="Black"
+            SelectedRowStyle-ForeColor="White"
+            DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="desc_materias" HeaderText="descripcion"  />
+                <asp:BoundField DataField="hs_semanales" HeaderText="horas semanales" />
+                <asp:BoundField DataField="hs_totales" HeaderText="horas totales" />
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="true" />
+
+            </Columns>
+        </asp:GridView>
+    </asp:Panel>
+    <asp:Panel ID="formPanel" Visible="false" runat="server">
+        <asp:Label ID="descLabel" runat="server" Text="Descripcion: "></asp:Label>
+        <asp:TextBox ID="descTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="hsemLabel" runat="server" Text="Horas semanales: "></asp:Label> 
+        <asp:TextBox ID="hsemTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="htotLabel" runat="server" Text="Horas totales: "></asp:Label>
+        <asp:TextBox ID="htotTextBox" runat="server"></asp:TextBox>
+        <br />
+        <asp:Panel ID="formActionsPanel" runat="server">
+             <asp:LinkButton ID="LinkButton1" runat="server" OnClick="aceptarLinkButton_Click">Aceptar</asp:LinkButton>
+             <asp:LinkButton ID="LinkButton2" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+    <asp:Panel ID="gridActionsPanel" runat="server">
+        <asp:LinkButton ID="LinkButton3" runat="server" OnClick="editarLinkButton_Click">Editar</asp:LinkButton>
+        <asp:LinkButton ID="LinkButton4" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+        <asp:LinkButton ID="LinkButton5" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+    </asp:Panel>
+</asp:Content>
---
+++ UI.Web/Materias.aspx.cs
@@ -0,0 +1,213 @@
+﻿using Business.Logic;
+using Business.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI.Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                this.LoadGrid();
+            }
+
+        }
+
+        MateriaLogic _logic;
+
+        private MateriaLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new MateriaLogic();
+                }
+
+                return _logic;
+            }
+        }
+
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+
+
+
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+
+        private Materia Entity
+        {
+            get;
+            set;
+
+        }
+
+        private int SelectedID
+        {
+            get
+            {
+                if(this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySeleted
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.descTextBox.Text = this.Entity.Descripcion;
+            /*this.hsemTextBox.Text = this.Entity.HSSemanales;
+            this.htotTextBox.Text = this.Entity.HSTotales;
+            */
+            // INSERTAR INT EN TEXTBOX
+        }
+
+        protected void editarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySeleted)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void LoadEntity(Materia materia)
+        {
+            materia.Descripcion = this.descTextBox.Text;
+            //materia.HSSemanales = this.hsemTextBox.Text;
+            // IDEM TEXTBOX INT
+        }
+
+        private void SaveEntity(Materia materia)
+        {
+            this.Logic.Save(materia);
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Materia();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Materia();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+            this.formPanel.Visible = false;
+        }
+
+
+        private void EnableForm(bool enable)
+        {
+            this.descTextBox.Enabled = enable;
+            this.hsemTextBox.Enabled = enable;
+            this.htotTextBox.Enabled = enable;
+            
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySeleted)
+            {
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        private void ClearForm()
+        {
+            this.descTextBox.Text = string.Empty;
+            this.hsemTextBox.Text = string.Empty;
+            this.htotTextBox.Text = string.Empty;
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.ClearForm();
+            this.LoadGrid();
+            this.formPanel.Visible = false;
+        }
+    }
+
+
+
+
+    
+
+
+}
